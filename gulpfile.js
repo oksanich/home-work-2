190857f2b7d8769b883422bfbb019264fbc89610
oksanich <oksanich_90@mail.ru>
2014-12-26 22:02:43 +0200

    Create scss file and add some plagin to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,49 @@
 
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
-    opn = require('opn');
+    opn = require('opn'),
+    wiredep = require('wiredep').stream,
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css'),
+    clean = require('gulp-clean');
 
+//Clear dist
+//gulp.task('clean', function () {
+//    return gulp.src('dist/*')
+//        .pipe(clean({force: true}))
+//        .pipe(gulp.dest('dist'));
+//});
+
+gulp.task('clean', function () {
+    return gulp.src('app/tmp', {read: false})
+        .pipe(gulp.dest('dist'));
+});
+
+// Assembly project
+gulp.task('dist', function () {
+    var assets = useref.assets();
+
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+//Work with bower
+gulp.task('wiredep', function () {
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            directory: 'app/bower_component'
+        }))
+        .pipe(gulp.dest('app'));
+});
+
+//Server
 gulp.task('connect', function() {
     connect.server({
         root: 'app',
@@ -35,6 +76,7 @@ gulp.task('watch', function(){
     gulp.watch(['app/*.html'], ['html']);
     gulp.watch(['app/css/*.css'], ['css']);
     gulp.watch(['app/js/*.js'], ['js']);
+    gulp.watch('bower.json', ['wiredep']);
 })
 
 //default
