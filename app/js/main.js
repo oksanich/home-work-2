340e34585823f6f6db2529969715f039fc4c1e23
oksanich <oksanich_90@mail.ru>
2014-12-28 14:49:00 +0200

    Add scroll-up

--- app/js/main.js
+++ app/js/main.js
@@ -18,4 +18,21 @@ $(document).ready(function(){
             });
         }
    });
+
+    //fade in/out based on scrollTop value
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.scroll-up').fadeIn();
+        } else {
+            $('.scroll-up').fadeOut();
+        }
+    });
+
+    // scroll body to 0px on click
+    $('.scroll-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
 });
